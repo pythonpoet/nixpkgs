8f3431219e67a61b3039b45c1490c5a04a21b1ed
Your Name <you@example.com>
2025-11-09 11:07:56 +0100

    update libconfig 1.8 < 1.8.1

--- pkgs/by-name/li/libconfig/package.nix
+++ pkgs/by-name/li/libconfig/package.nix
@@ -10,23 +10,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libconfig";
-  version = "1.8";
+  version = "1.8.1";
 
   src = fetchurl {
     url = "https://hyperrealm.github.io/libconfig/dist/libconfig-${finalAttrs.version}.tar.gz";
-    hash = "sha256-BR4V3Q6QfESQXzF5M/VIcxTypW6MZybIMEzpkIhIUKo=";
+    hash = "";
   };
-
-  patches = [
-    # Fix tests on i686-linux:
-    #   https://github.com/hyperrealm/libconfig/pull/260
-    # TODO: remove with a next release
-    (fetchpatch {
-      name = "32-bit-tests.patch";
-      url = "https://github.com/hyperrealm/libconfig/commit/b90c45a18110fcca415d00a98ff79c908c42544b.patch";
-      hash = "sha256-8CihXbpKx0Rn0CFxyP6+f6m8vUYehCl/1EtTo98VpfM=";
-    })
-  ];
 
   inherit doCheck;
 
